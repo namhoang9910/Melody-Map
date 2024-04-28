2818e365723c94a8e7626724c048834026b48986
namhoang9910 <namhoang9910@gmail.com>
2024-04-28 18:45:45 +1000

    UI: implemented & linked 4 fragments of bottom nav bar

---
+++ app/src/main/java/com/example/melodymap/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.melodymap;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.melodymap.databinding.ActivityMainBinding;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new ExploreFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            /*switch (item.getItemId()) {
+                case R.id.explore:
+                    replaceFragment(new ExploreFragment());
+                    break;
+                case R.id.myEvents:
+                    replaceFragment(new MyEventsFragment());
+                    break;
+                case R.id.inbox:
+                    replaceFragment(new InboxFragment());
+                    break;
+                case R.id.account:
+                    replaceFragment(new AccountFragment());
+                    break;
+            }*/
+            int itemId = item.getItemId();
+            if (itemId == R.id.explore) {
+                replaceFragment(new ExploreFragment());
+            } else if (itemId == R.id.myEvents) {
+                replaceFragment(new MyEventsFragment());
+            } else if (itemId == R.id.inbox) {
+                replaceFragment(new InboxFragment());
+            } else if (itemId == R.id.account) {
+                replaceFragment(new AccountFragment());
+            }
+
+            return true;
+        });
+
+
+        EdgeToEdge.enable(this);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+}
