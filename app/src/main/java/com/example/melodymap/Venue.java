34b593c5e99f68d26f666d9471dafce54da480bd
Dozer-spec <harunhadziahmetovic@gmail.com>
2024-05-12 14:30:12 +1000

    Adding Venue class

---
+++ app/src/main/java/com/example/melodymap/Venue.java
@@ -0,0 +1,79 @@
+package com.example.melodymap;
+
+import com.google.firebase.firestore.GeoPoint;
+
+// Venue.java
+public class Venue {
+    private String email;
+    private String genre;
+    private GeoPoint location;
+    private String name;
+    private String openingHours;
+    private String ownerName;
+    private String phoneNumber;
+
+    public Venue() {}
+
+    public Venue(String email, String genre, GeoPoint location, String name, String openingHours, String ownerName, String phoneNumber) {
+        this.email = email;
+        this.genre = genre;
+        this.location = location;
+        this.name = name;
+        this.openingHours = openingHours;
+        this.ownerName = ownerName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public GeoPoint getLocation() {
+        return location;
+    }
+
+    public void setLocation(GeoPoint location) {
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOpeningHours() {
+        return openingHours;
+    }
+    public void setOpeningHours(String openingHours) {
+        this.openingHours = openingHours;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
+
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
+    repositories {
+        google()
+    }
     dependencies {
         classpath(libs.secrets.gradle.plugin)
         classpath(libs.google.services)
+        classpath ("com.google.gms:google-services:4.3.15")
     }
 }
 
